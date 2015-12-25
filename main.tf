7fd041a0745b9aa4c9690b88c14cfde5d41ab049
Taketo Nakasuji <t.nksj.e89+gh@gmail.com>
2015-12-26 08:14:49 +0900

    moved parameters to variables.tf

--- main.tf
+++ main.tf
@@ -6,92 +6,95 @@ provider "aws" {
   region = "${var.region}"
 }
 
-resource "aws_vpc" "OKB-API-VPC" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "vpc" {
+  cidr_block = "${var.vpc.cidr}"
   instance_tenancy = "default"
   enable_dns_support = "true"
   enable_dns_hostnames = "false"
   tags {
-    Name = "OKB-API-VPC"
+    Name = "${var.vpc.name}"
   }
 }
 
-resource "aws_internet_gateway" "OKB-API-IGW" {
-    vpc_id = "${aws_vpc.OKB-API-VPC.id}"
-}
-
-resource "aws_subnet" "Public-Subnet-1a" {
-    vpc_id = "${aws_vpc.OKB-API-VPC.id}"
-    cidr_block = "10.0.0.0/24"
-    availability_zone = "ap-northeast-1a"
+resource "aws_internet_gateway" "igw" {
+    vpc_id = "${aws_vpc.vpc.id}"
     tags {
-      Name = "Public-Subnet-1a"
+      Name = "${var.igw_name}"
     }
 }
 
-resource "aws_subnet" "Public-Subnet-1c" {
-    vpc_id = "${aws_vpc.OKB-API-VPC.id}"
-    cidr_block = "10.0.1.0/24"
+resource "aws_subnet" "public_subnet_1a" {
+    vpc_id = "${aws_vpc.vpc.id}"
+    cidr_block = "${var.public_subnet_1a.ip}"
+    availability_zone = "ap-northeast-1a"
+    tags {
+      Name = "${var.public_subnet_1a.name}"
+    }
+}
+
+resource "aws_subnet" "public_sunbnet_1c" {
+    vpc_id = "${aws_vpc.vpc.id}"
+    cidr_block = "${var.public_subnet_1c.ip}"
     availability_zone = "ap-northeast-1c"
     tags {
       Name = "Public-Subnet-1c"
     }
 }
 
-resource "aws_subnet" "Private-Subnet-1a" {
-    vpc_id = "${aws_vpc.OKB-API-VPC.id}"
-    cidr_block = "10.0.2.0/24"
+resource "aws_subnet" "private_subnet_1a" {
+    vpc_id = "${aws_vpc.vpc.id}"
+    cidr_block = "${var.private_subnet_1a.ip}"
     availability_zone = "ap-northeast-1a"
     tags {
-      Name = "Private-Subnet-1a"
+      Name = "${var.private_subnet_1a.name}"
     }
 }
 
-resource "aws_subnet" "Private-Subnet-1c" {
-    vpc_id = "${aws_vpc.OKB-API-VPC.id}"
-    cidr_block = "10.0.3.0/24"
+resource "aws_subnet" "private_subnet_1c" {
+    vpc_id = "${aws_vpc.vpc.id}"
+    cidr_block = "${var.private_subnet_1c.ip}"
     availability_zone = "ap-northeast-1c"
     tags {
-      Name = "Private-Subnet-1c"
+      Name = "${var.private_subnet_1c.name}"
     }
 }
 
-resource "aws_route_table" "Public-Route" {
-    vpc_id = "${aws_vpc.OKB-API-VPC.id}"
+resource "aws_route_table" "prod_pub_route" {
+    vpc_id = "${aws_vpc.vpc.id}"
     route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.OKB-API-IGW.id}"
+        cidr_block = "${var.prod_pub_route.cidr_block}"
+        gateway_id = "${aws_internet_gateway.igw.id}"
     }
 }
 
-resource "aws_route_table" "Private-Route" {
-    vpc_id = "${aws_vpc.OKB-API-VPC.id}"
+resource "aws_route_table" "prod_prv_ruoute" {
+    vpc_id = "${aws_vpc.vpc.id}"
 }
 
 resource "aws_route_table_association" "Public-Subnet-1a" {
-    subnet_id = "${aws_subnet.Public-Subnet-1a.id}"
-    route_table_id = "${aws_route_table.Public-Route.id}"
+    subnet_id = "${aws_subnet.public_subnet_1a.id}"
+    route_table_id = "${aws_route_table.prod_pub_route.id}"
 }
 
 resource "aws_route_table_association" "Public-Subnet-1c" {
-    subnet_id = "${aws_subnet.Public-Subnet-1c.id}"
-    route_table_id = "${aws_route_table.Public-Route.id}"
+    subnet_id = "${aws_subnet.public_sunbnet_1c.id}"
+    route_table_id = "${aws_route_table.prod_pub_route.id}"
 }
 
 resource "aws_route_table_association" "Private-Subnet-1a" {
-    subnet_id = "${aws_subnet.Private-Subnet-1a.id}"
-    route_table_id = "${aws_route_table.Private-Route.id}"
+    subnet_id = "${aws_subnet.private_subnet_1a.id}"
+    route_table_id = "${aws_route_table.prod_prv_ruoute.id}"
 }
 
 resource "aws_route_table_association" "Private-Subnet-1c" {
-    subnet_id = "${aws_subnet.Private-Subnet-1c.id}"
-    route_table_id = "${aws_route_table.Private-Route.id}"
+    subnet_id = "${aws_subnet.private_subnet_1c.id}"
+    route_table_id = "${aws_route_table.prod_prv_ruoute.id}"
 }
 
 resource "aws_security_group" "ssh" {
     name = "ssh"
     description = "Allow SSH inbound traffic"
-    vpc_id = "${aws_vpc.OKB-API-VPC.id}"
+    vpc_id = "${aws_vpc.vpc.id}"
     ingress {
         from_port = 22
         to_port = 22
--- variables.tf
+++ variables.tf
@@ -5,3 +5,55 @@ variable "aws_secret_key" {}
 variable "region" {
     default = "ap-northeast-1"
 }
+
+variable "vpc" {
+  default = {
+      name = "okb-api-vpc"
+      cidr = "10.0.0.0/8"
+  }
+}
+
+variable "igw_name" {
+  default = "okb-api-igw"
+}
+
+variable "availability_zones" {
+    default = {
+        ap-northeast-1a = "ap-northeast-1a"
+        ap-northeast-1c = "ap-northeast-1c"
+    }
+}
+
+variable "public_subnet_1a" {
+  default = {
+    ip = "10.0.0.0/24"
+    name = "public_subnet_1a"
+  }
+}
+
+variable "public_subnet_1c" {
+  default = {
+    ip = "10.0.1.0/24"
+    name = "public_subnet_1c"
+  }
+}
+
+variable "private_subnet_1a" {
+  default = {
+    ip = "10.0.2.0/24"
+    name = "public_subnet_1c"
+  }
+}
+
+variable "private_subnet_1c" {
+  default = {
+    ip = "10.0.3.0/24"
+    name = "public_subnet_1c"
+  }
+}
+
+variable "prod_pub_route" {
+  default = {
+    cidr_block "0.0.0.0/0"
+  }
+}
